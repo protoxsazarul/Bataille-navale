f4b4c6e79220adeaaddb4989fc6f763dd273fb90
Quentin <Quentincnudde6@gmail.com>
2019-06-26 12:23:21 +0200

    first commit uml part 1

---
+++ src/Boat.java
@@ -0,0 +1,7 @@
+public class Boat extends BoatSpot {
+    private Boolean isSinked;
+
+    public boolean setIsSinked() {
+        return false;
+    }
+}
---
+++ src/Spot.java
@@ -0,0 +1,13 @@
+public class Spot extends Grid {
+    private static Boolean isTouched;
+    public static Boolean getIsTouched() {
+        return isTouched;
+    }
+
+    public static void setIsTouched(Boolean isTouched) {
+        Spot.isTouched = isTouched;
+    }
+
+
+
+}
